dee8ed541a936adb067ccf52cca289f7b148336b
성찬우 <fastchanoo@gmail.com>
2023-04-26 20:21:41 +0900

    /api/users/{id} 본인 인증 추가

--- src/main/kotlin/app/klock/api/functional/account/AccountHandler.kt
+++ src/main/kotlin/app/klock/api/functional/account/AccountHandler.kt
@@ -4,74 +4,85 @@ import app.klock.api.domain.entity.Account
 import app.klock.api.domain.entity.AccountRole
 import app.klock.api.functional.auth.dto.*
 import app.klock.api.service.AccountService
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.LocalDateTime
 
 @Component
 class AccountHandler(private val accountService: AccountService) {
 
-    // 사용자 목록 조회
-    fun getAllUsers(request: ServerRequest): Mono<ServerResponse> {
-        val usersFlux = accountService.findAll()
-        return usersFlux.map { user -> UserResponse(user.id, user.username, user.email) }
-            .collectList()
-            .flatMap { users -> ServerResponse.ok().body(BodyInserters.fromValue(users)) }
-    }
+  // 사용자 목록 조회
+  fun getAllUsers(request: ServerRequest): Mono<ServerResponse> {
+    val usersFlux = accountService.findAll()
+    return usersFlux.map { user -> UserResponse(user.id, user.username, user.email) }
+      .collectList()
+      .flatMap { users -> ServerResponse.ok().body(BodyInserters.fromValue(users)) }
+  }
 
-    // 사용자 상세 조회
-    fun getUserById(request: ServerRequest): Mono<ServerResponse> =
-        accountService.findById(request.pathVariable("id").toLong())
+  // 사용자 상세 조회
+  fun getUserById(request: ServerRequest): Mono<ServerResponse> {
+    val requestedUserId = request.pathVariable("id").toLong()
+
+    return request.principal()
+      .flatMap { principal ->
+        if (principal.name != requestedUserId.toString()) {
+          ServerResponse.status(HttpStatus.FORBIDDEN)
+            .bodyValue(mapOf("error" to "You are not authorized to access this user information."))
+        } else {
+          accountService.findById(requestedUserId)
             .flatMap { user ->
-                ServerResponse.ok().bodyValue(UserResponse(user.id, user.username, user.email))
+              ServerResponse.ok().bodyValue(UserResponse(user.id, user.username, user.email))
             }
             .switchIfEmpty(ServerResponse.notFound().build())
+        }
+      }
+  }
 
-    // 사용자 수정
-    fun updateUser(request: ServerRequest): Mono<ServerResponse> =
-        request.bodyToMono(UpdateUserRequest::class.java)
-            .flatMap { userRequest ->
-                val userId = request.pathVariable("id").toLong()
-                accountService.update(
-                    id = userId,
-                    user = Account(
-                        id = userRequest.id,
-                        username = userRequest.name,
-                        email = userRequest.email,
-                        hashedPassword = userRequest.password,
-                        role = AccountRole.USER,
-                        active = true,
-                        totalStudyTime = 0,
-                        accountLevelId = 1,
-                        createdAt = LocalDateTime.now(),
-                        updatedAt = LocalDateTime.now()
-                    )
-                )
-            }
-            .flatMap { user ->
-                ServerResponse.ok().bodyValue(UpdateUserResponse(user.id, user.username, user.email))
-            }
-            .switchIfEmpty(ServerResponse.notFound().build())
+  // 사용자 수정
+  fun updateUser(request: ServerRequest): Mono<ServerResponse> =
+    request.bodyToMono(UpdateUserRequest::class.java)
+      .flatMap { userRequest ->
+        val userId = request.pathVariable("id").toLong()
+        accountService.update(
+          id = userId,
+          user = Account(
+            id = userRequest.id,
+            username = userRequest.name,
+            email = userRequest.email,
+            hashedPassword = userRequest.password,
+            role = AccountRole.USER,
+            active = true,
+            totalStudyTime = 0,
+            accountLevelId = 1,
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now()
+          )
+        )
+      }
+      .flatMap { user ->
+        ServerResponse.ok().bodyValue(UpdateUserResponse(user.id, user.username, user.email))
+      }
+      .switchIfEmpty(ServerResponse.notFound().build())
 
-    // 사용자 삭제
-    fun deleteUser(request: ServerRequest): Mono<ServerResponse> =
-        accountService.deleteById(request.pathVariable("id").toLong())
-            .then(ServerResponse.noContent().build())
+  // 사용자 삭제
+  fun deleteUser(request: ServerRequest): Mono<ServerResponse> =
+    accountService.deleteById(request.pathVariable("id").toLong())
+      .then(ServerResponse.noContent().build())
 
-    // 계정 비밀번호 변경
-    fun changePassword(request: ServerRequest): Mono<ServerResponse> =
-        request.bodyToMono(ChangePasswordRequest::class.java)
-            .flatMap { changePasswordRequest ->
-                val accountId = request.pathVariable("id").toLong()
-                accountService.changePassword(accountId, changePasswordRequest.currentPassword, changePasswordRequest.newPassword)
-            }
-            .flatMap { account ->
-                ServerResponse.ok().bodyValue(AccountResponse.from(account))
-            }
-            .switchIfEmpty(ServerResponse.notFound().build())
+  // 계정 비밀번호 변경
+  fun changePassword(request: ServerRequest): Mono<ServerResponse> =
+    request.bodyToMono(ChangePasswordRequest::class.java)
+      .flatMap { changePasswordRequest ->
+        val accountId = request.pathVariable("id").toLong()
+        accountService.changePassword(accountId, changePasswordRequest.currentPassword, changePasswordRequest.newPassword)
+      }
+      .flatMap { account ->
+        ServerResponse.ok().bodyValue(AccountResponse.from(account))
+      }
+      .switchIfEmpty(ServerResponse.notFound().build())
 
 }
