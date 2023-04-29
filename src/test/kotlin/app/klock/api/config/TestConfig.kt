4e53af80c10ff7c1a776d67a20aa984c560593a9
성찬우 <fastchanoo@gmail.com>
2023-04-29 20:31:48 +0900

    테스트 케이스 설정

--- src/test/kotlin/app/klock/api/config/TestConfig.kt
+++ src/test/kotlin/app/klock/api/config/TestConfig.kt
@@ -6,6 +6,9 @@ import app.klock.api.functional.chatBot.ChatBotRouter
 import app.klock.api.functional.echo.EchoRouter
 import app.klock.api.functional.studySession.StudySessionRouter
 import app.klock.api.functional.tag.TagRouter
+import app.klock.api.functional.timer.TimerExamRouter
+import app.klock.api.functional.timer.TimerFocusRouter
+import app.klock.api.functional.timer.TimerPomodoroRouter
 import app.klock.api.functional.timer.TimerRouter
 import app.klock.api.functional.user.UserRouter
 import app.klock.api.functional.userTag.UserTagRouter
@@ -18,6 +21,9 @@ class TestConfig(
   private val authRouter: AuthRouter,
   private val userRouter: UserRouter,
   private val timerRouter: TimerRouter,
+  private val timerPomodoroRouter: TimerPomodoroRouter,
+  private val timerExamRouter: TimerExamRouter,
+  private val timerFocusRouter: TimerFocusRouter,
   private val tagRouter: TagRouter,
   private val echoRouter: EchoRouter,
   private val userTagRouter: UserTagRouter,
@@ -35,6 +41,9 @@ class TestConfig(
         .andOther(studySessionRouter.studySessionRoutes())
         .andOther(echoRouter.echoRoutes())
         .andOther(timerRouter.timerRoutes())
+        .andOther(timerPomodoroRouter.timerRoutes())
+        .andOther(timerExamRouter.timerRoutes())
+        .andOther(timerFocusRouter.timerRoutes())
         .andOther(tagRouter.tagRoutes()) // TagRouter를 추가합니다.
         .andOther(chatBotRouter.chatBotRoutes()) // ChatBotRouter를 추가합니다.
         .andOther(friendRelationRouter.friendRelationRoutes())
