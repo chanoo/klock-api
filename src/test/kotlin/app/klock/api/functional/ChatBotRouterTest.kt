96eb1e6ea06cc351f162d4dc5861735f87dc91cb
성찬우 <fastchanoo@gmail.com>
2023-05-06 14:06:01 +0900

    챗봇 목록 가져오기 테스트 개발

--- src/test/kotlin/app/klock/api/functional/ChatBotRouterTest.kt
+++ src/test/kotlin/app/klock/api/functional/ChatBotRouterTest.kt
@@ -1,64 +1,67 @@
 package app.klock.api.functional
 
-import app.klock.api.config.TestConfig
-import app.klock.api.domain.entity.ChatBot
-import app.klock.api.service.ChatBotService
+import app.klock.api.functional.chatBot.ChatBotDTO
+import app.klock.api.functional.chatBot.ChatBotRouter
+import app.klock.api.handler.ChatBotHandler
+import io.mockk.coEvery
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
-import reactor.core.publisher.Flux
-import java.time.LocalDateTime
-import org.mockito.Mockito.`when` as mockitoWhen
+import org.springframework.web.reactive.function.server.ServerResponse
 
-@ExtendWith(SpringExtension::class)
-@SpringBootTest(classes = [TestConfig::class])
 @ActiveProfiles("test")
-class ChatBotRouterTest @Autowired constructor(
-  private val client: WebTestClient
-) {
-  @MockBean
-  private lateinit var chatBotService: ChatBotService
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ChatBotRouterTest {
+
+  private lateinit var chatbotRouter: ChatBotRouter
+  private val chatBotHandler = mockk<ChatBotHandler>()
+
+  @BeforeEach
+  fun setUp() {
+    chatbotRouter = ChatBotRouter(chatBotHandler)
+  }
 
   @Test
   fun `챗봇 목록 가져오기`() {
-    // Prepare test data
+
     val testChatBots = listOf(
-      ChatBot(
+      ChatBotDTO(
         id = 1L,
         subject = "국어",
         title = "문장의 비결, 국어 전문가 미희 선생님",
         name = "미희",
         chatBotImageUrl = "img_korean_teacher",
         persona = "안녕하세요, 국어 선생님 미희입니다! 맞춤법, 작문, 독해 등 국어의 모든 것을 함께 배워볼까요? 궁금한 것이 있다면 언제든지 질문해주세요!",
-        active = true,
-        createdAt = LocalDateTime.now(),
-        updatedAt = LocalDateTime.now()
       )
-      // 추가적인 테스트 데이터를 여기에 추가할 수 있습니다.
     )
 
-    // Mock the chatBotService
-    mockitoWhen(chatBotService.getByActiveChatBots(true)).thenReturn(Flux.fromIterable(testChatBots))
+    coEvery { chatBotHandler.getByActiveChatBots(any()) } coAnswers {
+      ServerResponse.ok().bodyValue(testChatBots)
+    }
+
+    val client = WebTestClient.bindToRouterFunction(chatbotRouter.chatBotRoutes()).build()
 
     // Test the GET API
     client.get()
-      .uri("/api/chatbots?active=true")
+      .uri("/api/chat-bots?active=true")
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
-      .expectStatus().isOk
-      .expectHeader().contentType(MediaType.APPLICATION_JSON)
-      .expectBody()
-      .jsonPath("$[0].id").isEqualTo(testChatBots[0].id!!.toInt())
-      .jsonPath("$[0].subject").isEqualTo(testChatBots[0].subject)
-      .jsonPath("$[0].title").isEqualTo(testChatBots[0].title)
-      .jsonPath("$[0].name").isEqualTo(testChatBots[0].name)
-      .jsonPath("$[0].chatBotImageUrl").isEqualTo(testChatBots[0].chatBotImageUrl)
-      .jsonPath("$[0].persona").isEqualTo(testChatBots[0].persona)
+      .expectBody(object : ParameterizedTypeReference<List<ChatBotDTO>>() {})
+      .value { actualChatBots ->
+        val actualChatBotDto = actualChatBots[0]
+
+        assertEquals(testChatBots[0].id, actualChatBotDto.id)
+        assertEquals(testChatBots[0].subject, actualChatBotDto.subject)
+        assertEquals(testChatBots[0].title, actualChatBotDto.title)
+        assertEquals(testChatBots[0].name, actualChatBotDto.name)
+        assertEquals(testChatBots[0].chatBotImageUrl, actualChatBotDto.chatBotImageUrl)
+        assertEquals(testChatBots[0].persona, actualChatBotDto.persona)
+      }
   }
 }
