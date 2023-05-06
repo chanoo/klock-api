a9aeb2711eb1734cfd19861e2998e5e354edd982
성찬우 <fastchanoo@gmail.com>
2023-05-07 01:05:40 +0900

    인증 서비스 테스트 코드 수정

--- src/test/kotlin/app/klock/api/service/AuthServiceTest.kt
+++ src/test/kotlin/app/klock/api/service/AuthServiceTest.kt
@@ -2,42 +2,35 @@ import app.klock.api.domain.entity.SocialLogin
 import app.klock.api.domain.entity.SocialProvider
 import app.klock.api.domain.entity.User
 import app.klock.api.domain.entity.UserRole
-import app.klock.api.functional.auth.dto.SocialLoginRequest
 import app.klock.api.repository.SocialLoginRepository
 import app.klock.api.repository.UserRepository
 import app.klock.api.service.AuthService
 import app.klock.api.utils.JwtUtils
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.ArgumentMatchers.*
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 import java.time.LocalDateTime
 
-@ExtendWith(MockitoExtension::class)
 class AuthServiceTest {
 
-  @Mock
   private lateinit var jwtUtils: JwtUtils
-
-  @Mock
   private lateinit var passwordEncoder: PasswordEncoder
-
-  @Mock
   private lateinit var userRepository: UserRepository
-
-  @Mock
   private lateinit var socialLoginRepository: SocialLoginRepository
-
   private lateinit var authService: AuthService
 
   @BeforeEach
   fun setUp() {
+    jwtUtils = mockk<JwtUtils>()
+    passwordEncoder = mockk<BCryptPasswordEncoder>()
+    userRepository = mockk<UserRepository>()
+    socialLoginRepository = mockk<SocialLoginRepository>()
+
     authService = AuthService(jwtUtils, passwordEncoder, userRepository, socialLoginRepository)
   }
 
@@ -64,38 +57,44 @@ class AuthServiceTest {
     )
 
     // User 생성 관련 mock
-    Mockito.`when`(passwordEncoder.encode(savedUser.hashedPassword)).thenReturn("encoded_password")
-    Mockito.`when`(userRepository.save(any(User::class.java))).thenReturn(Mono.just(savedUser))
+    every { passwordEncoder.encode(savedUser.hashedPassword) } returns "encoded_password"
+    every { userRepository.save(any()) } returns Mono.just(savedUser)
 
     // SocialLogin 생성 관련 mock
-    Mockito.`when`(socialLoginRepository.save(any(SocialLogin::class.java))).thenReturn(Mono.just(socialLogin))
+    every { socialLoginRepository.save(any()) } returns Mono.just(socialLogin)
 
     // User 생성 테스트
-    StepVerifier.create(authService.signup(
-      username = savedUser.username,
-      email = savedUser.email,
-      password = savedUser.hashedPassword))
+    StepVerifier.create(
+      authService.signup(
+        username = savedUser.username,
+        email = savedUser.email,
+        password = savedUser.hashedPassword
+      )
+    )
       .expectNext(savedUser)
       .verifyComplete()
 
     // SocialLogin 생성 테스트
-    StepVerifier.create(authService.createSocialLogin(
-      userId = savedUser.id!!,
-      provider = SocialProvider.APPLE,
-      providerUserId = "1234"))
+    StepVerifier.create(
+      authService.createSocialLogin(
+        userId = savedUser.id!!,
+        provider = SocialProvider.APPLE,
+        providerUserId = "1234"
+      )
+    )
       .expectNext(socialLogin)
       .verifyComplete()
   }
 
-
   @Test
-  fun `애플 인증`() {
-    val socialLoginRequest = SocialLoginRequest(accessToken = "apple_access_token")
+  fun `토큰 새로 고침`() {
+    val refreshToken = "refresh_token"
     val jwtToken = "jwt_token"
-    val userEmail = "apple@example.com"
+    val userId = "1"
     val user = User(
+      id = 1,
+      email = "user@example.com",
       username = "testuser",
-      email = "apple@example.com",
       hashedPassword = "password",
       role = UserRole.USER,
       active = true,
@@ -105,26 +104,13 @@ class AuthServiceTest {
       updatedAt = LocalDateTime.now()
     )
 
-    Mockito.`when`(jwtUtils.getUserIdFromToken(anyString())).thenReturn(userEmail)
-    Mockito.`when`(jwtUtils.generateToken(anyString(), anyList())).thenReturn(jwtToken)
-    Mockito.`when`(userRepository.findByEmail(anyString())).thenReturn(Mono.empty())
-    Mockito.`when`(userRepository.save(any(User::class.java))).thenReturn(Mono.just(user))
-
-    StepVerifier.create(authService.authenticateApple(Mono.just(socialLoginRequest)))
-      .expectNext(jwtToken)
-      .verifyComplete()
-  }
-
-  @Test
-  fun `토큰 새로 고침`() {
-    val refreshToken = "refresh_token"
-    val jwtToken = "jwt_token"
-
-    Mockito.`when`(jwtUtils.validateTokenAndGetUserId(anyString())).thenReturn(jwtToken)
-    Mockito.`when`(jwtUtils.generateToken(anyString(), anyList())).thenReturn(jwtToken)
+    every { jwtUtils.validateTokenAndGetUserId(refreshToken) } returns userId
+    every { jwtUtils.generateToken(userId, any()) } returns jwtToken
+    every { userRepository.findById(userId.toLong()) } returns Mono.just(user)
 
     StepVerifier.create(authService.refreshToken(refreshToken))
       .expectNext(jwtToken)
       .verifyComplete()
   }
+
 }
