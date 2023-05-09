a73d4efeb26266b6fcdd69721f972dd0a5cf4536
성찬우 <fastchanoo@gmail.com>
2023-05-09 11:22:56 +0900

    시험시간 타이머 생성, 수정 버그 수정

--- src/main/kotlin/app/klock/api/functional/timer/TimerExamHandler.kt
+++ src/main/kotlin/app/klock/api/functional/timer/TimerExamHandler.kt
@@ -7,21 +7,27 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.bodyToMono
 import reactor.core.publisher.Mono
+import java.time.LocalDateTime
 
 @Component
 class TimerExamHandler(private val timerExamService: TimerExamService) {
 
   // 시험시간 타이머 생성
   fun createExamTimer(request: ServerRequest): Mono<ServerResponse> {
-    return request.bodyToMono<TimerExamDto>().flatMap { timerDto ->
-      val timer = timerDto.toDomain()
-      timer.validate()
+    return request.principal().flatMap { principal ->
+      val userId = principal.name.toLong()
+      request.bodyToMono<TimerExamDto>().flatMap { timerDto ->
+        val timer = timerDto.toDomain().copy(
+          userId = userId
+        )
+        timer.validate()
 
-      timerExamService.create(timer).flatMap { createdTimer ->
-        val createdTimerDto = TimerExamDto.from(createdTimer)
-        ServerResponse.status(HttpStatus.CREATED).bodyValue(createdTimerDto)
+        timerExamService.create(timer).flatMap { createdTimer ->
+          val createdTimerDto = TimerExamDto.from(createdTimer)
+          ServerResponse.status(HttpStatus.CREATED).bodyValue(createdTimerDto)
+        }
       }
-    }
+    }.switchIfEmpty(ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Unauthorized"))
   }
 
   // 시험시간 타이머 수정
@@ -30,10 +36,16 @@ class TimerExamHandler(private val timerExamService: TimerExamService) {
 
     return timerExamService.get(timerId).flatMap { existingTimer ->
       request.bodyToMono<TimerExamDto>().flatMap { timerDto ->
-        val timer = timerDto.toDomain().copy(
-          id = timerId
+        val timer = existingTimer.copy(
+          name = timerDto.name,
+          seq = timerDto.seq,
+          startTime = timerDto.startTime,
+          duration = timerDto.duration,
+          questionCount = timerDto.questionCount,
+          updatedAt = LocalDateTime.now()
         )
         timer.validate()
+
         timerExamService.update(timer).flatMap { updatedTimer ->
           val updatedTimerDto = TimerExamDto.from(updatedTimer)
           ServerResponse.ok().bodyValue(updatedTimerDto)
