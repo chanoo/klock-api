b0abe792b5a9292a8fa061c634058fc4f09b7857
성찬우 <fastchanoo@gmail.com>
2023-05-09 11:16:30 +0900

    뽀모도로 타이머 생성, 수정, 삭제 버그 수정

--- src/main/kotlin/app/klock/api/functional/timer/TimerPomodoroHandler.kt
+++ src/main/kotlin/app/klock/api/functional/timer/TimerPomodoroHandler.kt
@@ -7,21 +7,27 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.bodyToMono
 import reactor.core.publisher.Mono
+import java.time.LocalDateTime
 
 @Component
 class TimerPomodoroHandler(private val timerPomodoroService: TimerPomodoroService) {
 
   // Pomodoro timer 생성
   fun createPomodoroTimer(request: ServerRequest): Mono<ServerResponse> {
-    return request.bodyToMono<TimerPomodoroDto>().flatMap { timerDto ->
-      val timer = timerDto.toDomain()
-      timer.validate()
+    return request.principal().flatMap { principal ->
+      val userId = principal.name.toLong()
+      request.bodyToMono<TimerPomodoroDto>().flatMap { timerDto ->
+        val timer = timerDto.toDomain().copy(
+          userId = userId
+        )
+        timer.validate()
 
-      timerPomodoroService.create(timer).flatMap { createdTimer ->
-        val createdTimerDto = TimerPomodoroDto.from(createdTimer)
-        ServerResponse.status(HttpStatus.CREATED).bodyValue(createdTimerDto)
+        timerPomodoroService.create(timer).flatMap { createdTimer ->
+          val createdTimerDto = TimerPomodoroDto.from(createdTimer)
+          ServerResponse.status(HttpStatus.CREATED).bodyValue(createdTimerDto)
+        }
       }
-    }
+    }.switchIfEmpty(ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Unauthorized"))
   }
 
   // Pomodoro timer 수정
@@ -30,8 +36,13 @@ class TimerPomodoroHandler(private val timerPomodoroService: TimerPomodoroServic
 
     return timerPomodoroService.get(timerId).flatMap { existingTimer ->
       request.bodyToMono<TimerPomodoroDto>().flatMap { timerDto ->
-        val timer = timerDto.toDomain().copy(
-          id = timerId
+        val timer = existingTimer.copy(
+          name = timerDto.name,
+          seq = timerDto.seq,
+          focusTime = timerDto.focusTime,
+          restTime = timerDto.restTime,
+          cycleCount = timerDto.cycleCount,
+          updatedAt = LocalDateTime.now()
         )
         timer.validate()
 
@@ -51,8 +62,8 @@ class TimerPomodoroHandler(private val timerPomodoroService: TimerPomodoroServic
       if (isDeleted) {
         ServerResponse.status(HttpStatus.NO_CONTENT).build()
       } else {
-        ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue("Pomodoro timer not found")
+        ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue("Focus timer not found")
       }
-    }
+    }.switchIfEmpty(ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Unauthorized"))
   }
 }
