3919b3969ea574666ec12a6627bf8f771f036c4d
steve <hojungj@gmail.com>
2023-05-22 19:56:02 +0900

    타이머별 공부시간 세션 구분해서 등록 처리 - StudySession에 timerName, timerType 추가

--- src/main/kotlin/app/klock/api/functional/studySession/StudySessionDto.kt
+++ src/main/kotlin/app/klock/api/functional/studySession/StudySessionDto.kt
@@ -1,10 +1,27 @@
-package app.klock.api.functional.studySession.dto
+package app.klock.api.functional.studySession
 
+import app.klock.api.domain.entity.StudySession
+import app.klock.api.functional.timer.TimerType
 import java.time.LocalDateTime
 
-data class StudySessionDTO(
+data class StudySessionDto(
   val id: Long? = null,
   val userId: Long,
   val startTime: LocalDateTime,
   val endTime: LocalDateTime,
-)
+  val timerName: String,
+  val timerType: TimerType
+) {
+  fun toDomain() = StudySession(id, userId, startTime, endTime, timerName, timerType)
+
+  companion object {
+    fun from(domain: StudySession) = StudySessionDto(
+      domain.id,
+      domain.userId,
+      domain.startTime,
+      domain.endTime,
+      domain.timerName,
+      domain.timerType
+    )
+  }
+}
